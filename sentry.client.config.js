5f585506c0aaa89496d092ba82bd2fad829a60a7
Mohammed Azhar Padariyakam <azharakbar@gmail.com>
2021-12-30 02:23:36 +0530

    [SAS-7] :: Pass environment info to Sentry This will help us to filter issues in Sentry portal

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -12,6 +12,7 @@ Sentry.init({
     dsn: SENTRY_DSN,
     /* Adjust this value in production, or use tracesSampler for greater control */
     tracesSampleRate: 1.0,
+    environment: process.env.NEXT_PUBLIC_ENVIRONMENT || 'undefined-env',
     /**
      * Note: if you want to override the automatic release value, do not set a
      * `release` value here - use the environment variable `SENTRY_RELEASE`, so
