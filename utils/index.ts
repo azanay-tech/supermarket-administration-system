95b32e999b7a546d3800b98090fb65ad6f985558
Mohammed Azhar Padariyakam <azharakbar@gmail.com>
2021-12-30 17:07:30 +0530

    [SAS-22] [util] [compare-strings] :: Util to compare string equality Comparison can be case-insensitive or not based on parameter to function

--- utils/index.ts
+++ utils/index.ts
@@ -11,4 +11,27 @@ export const bool = (value: string | undefined | null): boolean =>
         value.toLowerCase() === 'on' ||
         value.toLowerCase() === '1');
 
+/**
+ * Compares the equality of two strings.
+ * Case sensitivity of comparison can be controlled by caseInsensitive parameter.
+ *
+ * @param stringOne Input String
+ * @param stringTwo Input String to compare with stringOne
+ * @param caseInSensitive Whether comparison should be case insensitive or not [default: true]
+ * @returns true if stringOne and stringTwo are equal
+ */
+export const compareStrings = (
+    stringOne: string | undefined | null,
+    stringTwo: string | undefined | null,
+    caseInSensitive: boolean = true
+): boolean => {
+    if (!stringOne || !stringTwo) {
+        return false;
+    }
+
+    return caseInSensitive
+        ? stringOne.toLowerCase() === stringTwo.toLowerCase()
+        : stringOne === stringTwo;
+};
+
 export default {};
