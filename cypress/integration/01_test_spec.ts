6c5cecf10f0bccd3ba7641c43f917682996ee8d5
Mohammed Azhar Padariyakam <azharakbar@gmail.com>
2022-01-15 19:06:51 +0530

    [SAS-9] :: Custom command to get by data-testid

--- cypress/integration/01_test_spec.ts
+++ cypress/integration/01_test_spec.ts
@@ -3,7 +3,7 @@
 context('Run a sample test suite', () => {
     it('Sample Testcase', () => {
         cy.visit('/');
-        cy.get('[data-testid="app_container"').should('have.length', 1);
-        cy.get('[data-testid="link_block"').should('have.length', 4);
+        cy.getByTestId('app_container').should('have.length', 1);
+        cy.getByTestId('link_block').should('have.length', 4);
     });
 });
